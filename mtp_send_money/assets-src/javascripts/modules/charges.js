3849caf79f902e93ae7271f1e56103388f78f7bf
Igor <igor.ushkarev@digital.justice.gov.uk>
2016-01-29 10:30:00 +0000

    Apply the new rounding algorithm for service charge also limit the form to only accept amounts of 10 chars or less to limit chances of floating-point errors

--- mtp_send_money/assets-src/javascripts/modules/charges.js
+++ mtp_send_money/assets-src/javascripts/modules/charges.js
@@ -11,8 +11,8 @@ exports.Charges = {
     var $noJsSection = $(this.selector + ' .mtp-charges-no-js');
     var $input = $(this.selector + ' .mtp-charges-amount');
 
-    this.percentageCharge = $input.data('percentage-charge');
-    this.fixedCharge = $input.data('fixed-charge');
+    this.percentageCharge = Number($input.data('percentage-charge'))/100;
+    this.fixedCharge = Number($input.data('fixed-charge'));
 
     this.$charges = $(this.selector + ' .mtp-charges-charges');
     this.$total = $(this.selector + ' .mtp-charges-total span');
@@ -30,15 +30,22 @@ exports.Charges = {
   },
 
   _formatAsPrice: function(num) {
-    return '£' + num.toFixed(2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+    return '£' + num.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  },
+
+  // Applies the rounding algorithm we use:
+  // 1. discard all decimal digits after the 3rd place (excluded)
+  // 2. round up
+  _round: function(num) {
+    num = num*100;
+    return Math.ceil(Number(num.toString().replace(/(\.\d)\d*/, '$1')))/100;
   },
 
   _updateTotal: function (amount) {
     if (/^ *\d+(\.\d{2})? *$/.test(amount)) {
-      amount = Number(amount);
-      var serviceCharge = (amount * this.percentageCharge + this.fixedCharge) / 100;
+      var serviceCharge = this._round(Number(amount) * this.percentageCharge + this.fixedCharge);
       this.$charges.text(this._formatAsPrice(serviceCharge));
-      this.$total.text(this._formatAsPrice(amount + serviceCharge));
+      this.$total.text(this._formatAsPrice(Number(amount) + this._round(serviceCharge)));
     } else {
       this.$charges.text('');
       this.$total.text('');
