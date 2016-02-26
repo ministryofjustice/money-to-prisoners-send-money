a3b4c02b77b90395dc33c9b2dd4f62a1085b849b
Ian Brechin <ian.brechin@gmail.com>
2016-02-26 16:24:24 +0000

    Add email field to citizen facing feedback page
    This allows us to follow up with the submitter should they wish
    it.

---
+++ mtp_send_money/apps/feedback/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+from django.utils.translation import ugettext_lazy as _
+from zendesk_tickets.forms import BaseTicketForm
+
+
+class CitizenFeedbackForm(BaseTicketForm):
+    ticket_content = forms.CharField(
+        label=_('Tell us what you think about this service and help us improve.'),
+        widget=forms.Textarea
+    )
+    contact_email = forms.EmailField(
+        label=_('Your email address'), required=False
+    )
+
+    def submit_ticket(self, request, subject, tags,
+                      ticket_template_name, extra_context={}):
+        extra_context = dict(extra_context, **{
+            'user_agent': request.META.get('HTTP_USER_AGENT')
+        })
+        return super().submit_ticket(request, subject, tags,
+                                     ticket_template_name, extra_context)
