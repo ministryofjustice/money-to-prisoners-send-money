94611e96a7916e4439b7aec57b886124e53dae08
Igor <igor.ushkarev@digital.justice.gov.uk>
2016-04-01 14:41:15 +0100

    Move common feedback form fields into Zendesk repo

--- mtp_send_money/apps/feedback/forms.py
+++ mtp_send_money/apps/feedback/forms.py
@@ -1,23 +1,10 @@
-from django import forms
 from django.conf import settings
-from django.utils.translation import ugettext_lazy as _
-from zendesk_tickets.forms import BaseTicketForm
+from zendesk_tickets.forms import EmailTicketForm
 
 
-class CitizenFeedbackForm(BaseTicketForm):
-    ticket_content = forms.CharField(
-        label=_('Enter your feedback or any questions you have about this service.'),
-        widget=forms.Textarea
-    )
-    contact_email = forms.EmailField(
-        label=_('Your email address'), required=False
-    )
-
+class CitizenFeedbackForm(EmailTicketForm):
     def submit_ticket(self, request, subject, tags,
                       ticket_template_name, extra_context={}):
-        extra_context = dict(extra_context, **{
-            'user_agent': request.META.get('HTTP_USER_AGENT')
-        })
         referer = self.cleaned_data.get('referer', None)
         if referer and settings.CITIZEN_INFO_URL in referer:
             tags += ['citizen-info']
