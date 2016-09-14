f872ca1c6b7dd73d15871e793b65d018a1fcc2ad
Vicky Gray <vicky.gray@digital.justice.gov.uk>
2016-09-14 17:14:34 +0100

    Add content and basic design for choice page

--- mtp_send_money/assets-src/javascripts/main.js
+++ mtp_send_money/assets-src/javascripts/main.js
@@ -6,7 +6,8 @@
   require('analytics').Analytics.init();
   require('element-focus').ElementFocus.init();
   require('year-field-completion').YearFieldCompletion.init();
-
+  require('help-popup').HelpPopup.init();
+  
   require('charges').Charges.init();
   require('experiments').Experiments.init();
 
