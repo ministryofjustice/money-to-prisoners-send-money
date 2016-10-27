a5c0f2f6d6f962d73fb616193a32a742a2ed5a31
Igor <igor.ushkarev@digital.justice.gov.uk>
2016-10-27 17:28:50 +0100

    Remove vestigial feedback templates and content header styling

--- mtp_send_money/apps/feedback/urls.py
+++ mtp_send_money/apps/feedback/urls.py
@@ -9,13 +9,13 @@ urlpatterns = [
     url(r'^feedback/$', views.ticket,
         {
             'form_class': CitizenFeedbackForm,
-            'template_name': 'feedback/submit_feedback.html',
+            'template_name': 'mtp_common/feedback/submit_feedback.html',
             'success_redirect_url': reverse_lazy('feedback_success'),
             'subject': 'MTP Send Money Feedback',
             'tags': ['feedback', 'mtp', 'send-money', settings.ENVIRONMENT]
         }, name='submit_ticket'),
     url(r'^feedback/success/$', views.success,
         {
-            'template_name': 'feedback/success.html',
+            'template_name': 'mtp_common/feedback/success.html',
         }, name='feedback_success'),
 ]
