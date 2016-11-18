b4ffb6577b360ba44c3d4409bba5c3e474408511
Igor <igor.ushkarev@digital.justice.gov.uk>
2016-11-18 14:46:01 +0000

    Set new start page URL

--- mtp_send_money/apps/feedback/forms.py
+++ mtp_send_money/apps/feedback/forms.py
@@ -3,10 +3,8 @@ from zendesk_tickets.forms import EmailTicketForm
 
 
 class CitizenFeedbackForm(EmailTicketForm):
-    def submit_ticket(self, request, subject, tags,
-                      ticket_template_name, extra_context={}):
-        referer = self.cleaned_data.get('referer', None)
-        if referer and settings.CITIZEN_INFO_URL in referer:
+    def submit_ticket(self, request, subject, tags, ticket_template_name, requester_email=None, extra_context={}):
+        if settings.START_PAGE_URL in (self.cleaned_data.get('referer') or ''):
             tags += ['citizen-info']
-        return super().submit_ticket(request, subject, tags,
-                                     ticket_template_name, extra_context)
+        return super().submit_ticket(request, subject, tags, ticket_template_name,
+                                     requester_email=requester_email, extra_context=extra_context)
