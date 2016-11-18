d9a138e926e69f90e9214cee596210c1cb2a29d6
Igor <igor.ushkarev@digital.justice.gov.uk>
2016-11-18 16:29:14 +0000

    Remove specialised feedback support for start page since the start page on gov.uk doesn’t have a link for contacting MTP

--- mtp_send_money/apps/feedback/urls.py
+++ mtp_send_money/apps/feedback/urls.py
@@ -2,17 +2,16 @@ from django.conf import settings
 from django.conf.urls import url
 from django.core.urlresolvers import reverse_lazy
 from zendesk_tickets import views
-
-from .forms import CitizenFeedbackForm
+from zendesk_tickets.forms import EmailTicketForm
 
 urlpatterns = [
     url(r'^feedback/$', views.ticket,
         {
-            'form_class': CitizenFeedbackForm,
+            'form_class': EmailTicketForm,
             'template_name': 'mtp_common/feedback/submit_feedback.html',
             'success_redirect_url': reverse_lazy('feedback_success'),
             'subject': 'MTP Send Money Feedback',
-            'tags': ['feedback', 'mtp', 'send-money', settings.ENVIRONMENT]
+            'tags': ['feedback', 'mtp', 'send-money', settings.ENVIRONMENT],
         }, name='submit_ticket'),
     url(r'^feedback/success/$', views.success,
         {
