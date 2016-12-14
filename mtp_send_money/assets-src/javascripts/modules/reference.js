2f0fd583d789c5f73bd31bf86048a50b574f87c4
Igor <igor.ushkarev@digital.justice.gov.uk>
2016-12-14 13:30:14 +0000

    Ignore print errors e.g. MS Edge on Win10 throws an exception on cancellation

--- mtp_send_money/assets-src/javascripts/modules/reference.js
+++ mtp_send_money/assets-src/javascripts/modules/reference.js
@@ -5,8 +5,11 @@ exports.Reference = {
   init: function () {
     $('.mtp-print-reference').click(function (e) {
       e.preventDefault();
-      window.print();
+      try {
+        window.print();
+      } catch (e) {}  // eslint-disable-line
     });
+
     $('.mtp-email-reference').click(function (e) {
       e.preventDefault();
       $('#mtp-email-reference-form').show();
