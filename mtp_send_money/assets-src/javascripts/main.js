a9999744d782ec3d2ed674908f2a8c98a3f9303c
Igor <igor.ushkarev@digital.justice.gov.uk>
2017-04-18 15:30:39 +0100

    Make user agent check whether it can connect to GOV.UK Pay before choice is made and improve JS compatibility in IE8

--- mtp_send_money/assets-src/javascripts/main.js
+++ mtp_send_money/assets-src/javascripts/main.js
@@ -1,6 +1,9 @@
 (function () {
   'use strict';
 
+  require('polyfills').Polyfills.init();
+  require('placeholder-polyfill').PlaceholderPolyfill.init();
+
   require('sticky-contents').StickyContents.init();
   require('proposition-header').PropositionHeader.init();
   require('analytics').Analytics.init();
@@ -9,9 +12,9 @@
   require('help-popup').HelpPopup.init();
   require('print').Print.init();
   require('charges').Charges.init();
+  require('govuk-pay-connection-check').GOVUKPayConnectionCheck.init();
   require('experiments').Experiments.init();
   require('reference').Reference.init();
   require('filtered-list').FilteredList.init();
-  require('placeholder-polyfill').PlaceholderPolyfill.init();
   require('selection-buttons').SelectionButtons.init();
 }());
--- mtp_send_money/settings/base.py
+++ mtp_send_money/settings/base.py
@@ -258,6 +258,10 @@ CONFIRMATION_EXPIRES = 60  # minutes
 
 GOVUK_PAY_URL = os.environ.get('GOVUK_PAY_URL', '')
 GOVUK_PAY_AUTH_TOKEN = os.environ.get('GOVUK_PAY_AUTH_TOKEN', '')
+GOVUK_PAY_CONNECTION_CHECK_IMAGE = os.environ.get(
+    'GOVUK_PAY_CONNECTION_CHECK_IMAGE',
+    'https://www.payments.service.gov.uk/public/images/icon-search.png',
+)
 
 EMAIL_BACKEND = 'django_mailgun.MailgunBackend'
 MAILGUN_ACCESS_KEY = os.environ.get('MAILGUN_ACCESS_KEY', '')
