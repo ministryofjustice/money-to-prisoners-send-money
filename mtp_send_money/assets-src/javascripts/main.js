d7e5b18283698659545d9f8aeaea3025e19a49a5
Igor <igor.ushkarev@digital.justice.gov.uk>
2017-05-23 16:21:28 +0100

    Update common dependency to use latest GOV.UK styles

--- mtp_send_money/assets-src/javascripts/main.js
+++ mtp_send_money/assets-src/javascripts/main.js
@@ -4,17 +4,17 @@
   require('polyfills').Polyfills.init();
   require('placeholder-polyfill').PlaceholderPolyfill.init();
 
-  require('sticky-contents').StickyContents.init();
   require('proposition-header').PropositionHeader.init();
   require('analytics').Analytics.init();
   require('element-focus').ElementFocus.init();
   require('year-field-completion').YearFieldCompletion.init();
   require('help-popup').HelpPopup.init();
   require('print').Print.init();
+
   require('charges').Charges.init();
   require('govuk-pay-connection-check').GOVUKPayConnectionCheck.init();
   require('experiments').Experiments.init();
   require('reference').Reference.init();
   require('filtered-list').FilteredList.init();
-  require('selection-buttons').SelectionButtons.init();
+  require('sticky-contents').StickyContents.init();
 }());
