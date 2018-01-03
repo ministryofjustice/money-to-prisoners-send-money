3fd0225653f07dfae9a90311f0beb95291e09a15
Igor <igor.ushkarev@digital.justice.gov.uk>
2018-01-03 13:36:40 +0000

    Update mtp-common dependency and get-help class-based views

--- mtp_send_money/apps/feedback/urls.py
+++ mtp_send_money/apps/feedback/urls.py
@@ -1,24 +1,36 @@
 from django.conf import settings
 from django.conf.urls import url
 from django.urls import reverse_lazy
+from django.utils.translation import gettext_lazy as _
 from django.views.generic import RedirectView
-from zendesk_tickets import views
+from mtp_common.views import GetHelpView as BaseGetHelpView, GetHelpSuccessView as BaseGetHelpSuccessView
 
 from feedback.forms import ContactForm
 
+
+class GetHelpView(BaseGetHelpView):
+    form_class = ContactForm
+    success_url = reverse_lazy('feedback_success')
+    template_name = 'send_money/contact-form.html'
+    ticket_subject = 'MTP Send Money Feedback'
+    ticket_tags = ['feedback', 'mtp', 'send-money', settings.ENVIRONMENT]
+    ticket_template_name = 'send_money/contact-form-ticket.txt'
+
+    def get_context_data(self, **kwargs):
+        kwargs['get_help_title'] = _('Contact us')
+        return super().get_context_data(**kwargs)
+
+
+class GetHelpSuccessView(BaseGetHelpSuccessView):
+    template_name = 'send_money/contact-form-success.html'
+
+    def get_context_data(self, **kwargs):
+        kwargs['get_help_title'] = _('Contact us')
+        return super().get_context_data(**kwargs)
+
+
 urlpatterns = [
     url(r'^feedback/$', RedirectView.as_view(url=reverse_lazy('submit_ticket'))),
-    url(r'^contact-us/$', views.ticket,
-        {
-            'form_class': ContactForm,
-            'template_name': 'send_money/contact-form.html',
-            'ticket_template_name': 'send_money/contact-form-ticket.txt',
-            'success_redirect_url': reverse_lazy('feedback_success'),
-            'subject': 'MTP Send Money Feedback',
-            'tags': ['feedback', 'mtp', 'send-money', settings.ENVIRONMENT],
-        }, name='submit_ticket'),
-    url(r'^contact-us/success/$', views.success,
-        {
-            'template_name': 'send_money/contact-form-success.html',
-        }, name='feedback_success'),
+    url(r'^contact-us/$', GetHelpView.as_view(), name='submit_ticket'),
+    url(r'^contact-us/success/$', GetHelpSuccessView.as_view(), name='feedback_success'),
 ]
