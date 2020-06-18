f265421ac90c458b506673040c9b166185fcc10a
Igor <igor.ushkarev@digital.justice.gov.uk>
2020-06-18 14:48:36 +0100

    Make it clear that new help views are not subclassed … rather they use init kwargs for customisation

--- mtp_send_money/apps/help_area/views.py
+++ mtp_send_money/apps/help_area/views.py
@@ -57,7 +57,7 @@ class ContactSuccessView(BaseGetHelpSuccessView):
 
 
 class HelpView(CacheableTemplateView):
-    back_url = NotImplemented
+    back_url = None
 
     def get_context_data(self, **kwargs):
         context_data = super().get_context_data(**kwargs)
