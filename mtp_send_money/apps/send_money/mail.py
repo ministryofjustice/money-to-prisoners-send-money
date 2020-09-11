87bc0a37d933d903464a58cf27a654d3b3dbe683
Igor <igor.ushkarev@digital.justice.gov.uk>
2020-09-11 17:02:51 +0100

    Use appropriate fallback if contact address for compliance team is missing i.e. top-level help page rather than generic contact form

--- mtp_send_money/apps/send_money/mail.py
+++ mtp_send_money/apps/send_money/mail.py
@@ -70,7 +70,7 @@ def send_email_for_card_payment_accepted(email, payment):
 
 def send_email_for_card_payment_rejected(email, payment):
     context = _get_email_context_for_payment(payment)
-    context['compliance_contact'] = settings.COMPLIANCE_CONTACT_EMAIL or site_url(reverse('help_area:submit_ticket'))
+    context['compliance_contact'] = settings.COMPLIANCE_CONTACT_EMAIL or site_url(reverse('help_area:help'))
 
     _send_notification_email(
         email,
