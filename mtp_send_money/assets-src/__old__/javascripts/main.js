50f1ea8bf8addcde184220cc6aab57a552081f72
Igor <igor.ushkarev@digital.justice.gov.uk>
2021-01-06 02:57:44 +0000

    Remove old source assets

--- mtp_send_money/assets-src/__old__/javascripts/main.js
+++
@@ -1,9 +0,0 @@
-(function () {
-  'use strict';
-
-  // send-money
-  require('charges').Charges.init();
-  require('reference').Reference.init();
-  require('filtered-list').FilteredList.init();
-  require('question-list').QuestionList.init();
-}());
