35eb9214f3e30beb40157f24287b8ee3cb1212e4
Igor <igor.ushkarev@digital.justice.gov.uk>
2022-06-08 14:59:42 +0100

    Collect request duration metrics for Prometheus

--- mtp_send_money/settings/base.py
+++ mtp_send_money/settings/base.py
@@ -71,6 +71,7 @@ INSTALLED_APPS += PROJECT_APPS
 WSGI_APPLICATION = 'mtp_send_money.wsgi.application'
 ROOT_URLCONF = 'mtp_send_money.urls'
 MIDDLEWARE = (
+    'mtp_common.metrics.middleware.RequestMetricsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
