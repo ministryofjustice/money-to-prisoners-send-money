26f038c3743b883a514a9e794629e77199e93bd6
Aldo "xoen" Giambelluca <aldo.giambelluca@digital.justice.gov.uk>
2025-02-26 15:16:18 +0000

    django.conf.urls.url() removed in Django 4.x
    Replaced with django.urls.re_path()

--- mtp_send_money/apps/help_area/urls.py
+++ mtp_send_money/apps/help_area/urls.py
@@ -1,6 +1,5 @@
 from django.conf import settings
-from django.conf.urls import url
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, re_path
 from django.views.generic import RedirectView
 
 from help_area import views
@@ -8,60 +7,60 @@ from help_area import views
 
 app_name = 'help_area'
 urlpatterns = [
-    url(r'^help/$',
+    re_path(r'^help/$',
         views.HelpView.as_view(
             template_name='help_area/help.html',
             back_url=settings.START_PAGE_URL,
         ),
         name='help'),
-    url(r'^help/with-making-a-payment/$',
+    re_path(r'^help/with-making-a-payment/$',
         views.HelpView.as_view(
             template_name='help_area/help-new-payment.html',
             back_url=reverse_lazy('help_area:help'),
         ),
         name='help-new-payment'),
-    url(r'^help/with-a-payment-i-sent/$',
+    re_path(r'^help/with-a-payment-i-sent/$',
         views.HelpView.as_view(
             template_name='help_area/help-sent-payment.html',
             back_url=reverse_lazy('help_area:help'),
         ),
         name='help-sent-payment'),
-    url(r'^help/cannot-access/$',
+    re_path(r'^help/cannot-access/$',
         views.HelpView.as_view(
             template_name='help_area/help-cannot-access.html',
             back_url=reverse_lazy('help_area:help'),
         ),
         name='help-cannot-access'),
-    url(r'^help/setup-basic-bank-account/$',
+    re_path(r'^help/setup-basic-bank-account/$',
         views.HelpView.as_view(
             template_name='help_area/help-setup-basic-bank-account.html',
             back_url=reverse_lazy('help_area:help-cannot-access'),
         ),
         name='help-setup-basic-bank-account'),
-    url(r'^help/apply-for-exemption/$',
+    re_path(r'^help/apply-for-exemption/$',
         views.HelpView.as_view(
             template_name='help_area/help-apply-for-exemption.html',
             back_url=reverse_lazy('help_area:help-cannot-access'),
         ),
         name='help-apply-for-exemption'),
 
-    url(r'^help/bank-transfer-issues/$',
+    re_path(r'^help/bank-transfer-issues/$',
         RedirectView.as_view(url=reverse_lazy('help_area:help-sent-payment'), permanent=True)),
-    url(r'^help/payment-delay/$',
+    re_path(r'^help/payment-delay/$',
         RedirectView.as_view(url=reverse_lazy('help_area:help-sent-payment'), permanent=True)),
-    url(r'^help/prisoner-moved-or-released/$',
+    re_path(r'^help/prisoner-moved-or-released/$',
         RedirectView.as_view(url=reverse_lazy('help_area:help-sent-payment'), permanent=True)),
 
-    url(r'^help/prisons/$', views.PrisonListView.as_view(), name='prison_list'),
+    re_path(r'^help/prisons/$', views.PrisonListView.as_view(), name='prison_list'),
 
-    url(r'^contact-us/$',
+    re_path(r'^contact-us/$',
         RedirectView.as_view(url=reverse_lazy('help_area:help'), permanent=False),
         name='submit_ticket'),
-    url(r'^contact-us/success/$', views.ContactSuccessView.as_view(), name='feedback_success'),
-    url(r'^help/with-making-a-payment/contact-us/$',
+    re_path(r'^contact-us/success/$', views.ContactSuccessView.as_view(), name='feedback_success'),
+    re_path(r'^help/with-making-a-payment/contact-us/$',
         views.ContactNewPaymentView.as_view(),
         name='contact-us-new-payment'),
-    url(r'^help/with-a-payment-i-sent/contact-us/$',
+    re_path(r'^help/with-a-payment-i-sent/contact-us/$',
         views.ContactSentPaymentView.as_view(),
         name='contact-us-sent-payment'),
 ]
